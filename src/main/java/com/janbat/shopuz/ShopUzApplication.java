3b460515d8af8603b73eaf6dd944ec80d899b4fe
Maksymilian Labuc <109258@g.elearn.uz.zgora.pl>
2024-10-17 20:49:41 +0200

    Initial commit

---
+++ src/main/java/com/janbat/shopuz/ShopUzApplication.java
@@ -0,0 +1,13 @@
+package com.janbat.shopuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janbat/shopuz/ShopUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janbat.shopuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
