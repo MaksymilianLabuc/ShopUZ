4fd770b580e5f40242b667ffce66277b614e5298
JO Krul <109252@g.elearn.uz.zgora.pl>
2024-11-06 23:40:03 +0100

    captcha dodana (statycznie klucz strony) + motywyw

---
+++ src/main/java/com/janbat/shopuz/CaptchaService.java
@@ -0,0 +1,33 @@
+package com.janbat.shopuz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class CaptchaService {
+    private final CaptchaSettings captchaSettings;
+    
+    @Autowired
+    public CaptchaService(CaptchaSettings captchaSettings) {
+        this.captchaSettings = captchaSettings;
+    }
+
+    public boolean verifyCaptcha(String response) {
+        String url = "https://www.google.com/recaptcha/api/siteverify";
+        RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("secret", captchaSettings.getSecret());
+        params.add("response", response);
+
+        ResponseEntity<Map> responseEntity = restTemplate.postForEntity(url, params, Map.class);
+        Map<String, Object> body = responseEntity.getBody();
+        return (Boolean) body.get("success");
+    }
+}
+
--- src/main/java/com/janbat/shopuz/controller/AuthController.java
+++ src/main/java/com/janbat/shopuz/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.janbat.shopuz.controller;
 
+import com.janbat.shopuz.CaptchaSettings;
 import com.janbat.shopuz.model.LoginForm;
 import com.janbat.shopuz.model.RegisterForm;
 import com.janbat.shopuz.model.User;
+import com.janbat.shopuz.CaptchaService;
 import com.janbat.shopuz.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,22 +14,36 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class AuthController {
 
+    private final UserService userService;
+    private final CaptchaService captchaService;
+    private final CaptchaSettings captchaSettings;
+
     @Autowired
-    private UserService userService;
+    public AuthController(UserService userService, CaptchaService captchaService, CaptchaSettings captchaSettings) {
+        this.userService = userService;
+        this.captchaService = captchaService;
+        this.captchaSettings = captchaSettings;
+    }
 
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("loginForm", new LoginForm());
+        model.addAttribute("captchaSettings", captchaSettings);
         return "login";
     }
     @PostMapping("/login")
-    public String loginUser(@Valid LoginForm loginForm, BindingResult bindingResult) {
+    public String loginUser(@RequestParam("g-recaptcha-response") String captchaResponse, @Valid LoginForm loginForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
+            return "login";
+        }
+        if (!captchaService.verifyCaptcha(captchaResponse)) {
+            model.addAttribute("captchaError", "Invalid Captcha");
             return "login";
         }
         // Logika logowania
@@ -36,12 +52,17 @@ public class AuthController {
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("registerForm", new RegisterForm());
+        model.addAttribute("captchaSettings", captchaSettings);
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute("registerForm") @Valid RegisterForm registerForm, BindingResult bindingResult) {
+    public String registerUser(@RequestParam("g-recaptcha-response") String captchaResponse, @ModelAttribute("registerForm") @Valid RegisterForm registerForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        if (!captchaService.verifyCaptcha(captchaResponse)) {
+            model.addAttribute("captchaError", "Invalid Captcha");
             return "register";
         }
         userService.registerNewUser(registerForm);
